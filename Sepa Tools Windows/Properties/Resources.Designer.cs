2b25e2291a946a026f2f28bb9f7900d0e0fcd9ac
dan <dan@DOCKSTATION>
2017-01-05 13:15:00 +0100

    minor framework version change

--- Sepa Tools Windows/Properties/Resources.Designer.cs
+++ Sepa Tools Windows/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SepaTools.Properties {
+namespace Sepa_Tools_Windows.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SepaTools.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SepaTools.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sepa_Tools_Windows.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
